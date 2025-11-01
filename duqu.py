319b45936ae54ff49b43c1240ddfd677039c5494
zhao bolei <357801@whut.edu.cn>
2025-11-01 03:37:19 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    .gitignore
    	modified:   README.md
    	deleted:    duqu.py
    	modified:   login.py
    	modified:   setting.yaml
    	deleted:    "\344\275\277\347\224\250\350\257\264\346\230\216.md"

--- duqu.py
+++
@@ -1,8 +0,0 @@
-import yaml
-with open('setting.yaml', 'r', encoding='utf-8') as f:
-    settings = yaml.safe_load(f)
-
-username = settings.get('账号')
-password = settings.get('密码')
-clash_path = settings.get('clash路径')
-print(username, password, clash_path)
