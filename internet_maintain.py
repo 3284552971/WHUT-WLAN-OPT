2b31061ad1efc8ae23493b3cad310ae43b8ea97c
zhaobolei <3284552971@qq.com>
2025-11-03 11:00:00 +0800

    Changes to be committed: 	modified:   internet_maintain.py 	modified:   setting.yaml 	new file:   start.bat

--- internet_maintain.py
+++ internet_maintain.py
@@ -3,17 +3,31 @@ from playwright.async_api import async_playwright
 import asyncio
 import yaml
 import requests
+from pathlib import Path
 
 class Network_maintain:
     def __init__(self):
-        with open("setting.yaml", 'r', encoding='utf-8') as f:
+        pass
+
+    async def setting(self):
+        with open(str(Path(__file__).parent / "setting.yaml"), 'r', encoding='utf-8') as f:
             self.config = yaml.safe_load(f)
         self.username = self.config.get('校园网账号')
         self.password = self.config.get('校园网密码')
-        self.check_interval = self.config.get('检测间隔', 60)
+        if not self.username or not self.password:
+            return False
+        self.check_interval = self.config.get('检测间隔', 300)
         self.url = self.config.get("url")
+        return True
 
     async def main_loop(self):
+        result = asyncio.run(self.setting())
+        if not result:
+            print("请先在setting.yaml中配置校园网账号和密码")
+        while not result:
+            await asyncio.sleep(30)
+            result = asyncio.run(self.setting())
+        print("网络维护程序已启动...")
         while True:
             check = await self.is_net_ok()
             if not check:
@@ -31,7 +45,6 @@ class Network_maintain:
             await page.fill('#password', self.password)
             await page.click('#login-account')
             await page.wait_for_timeout(5000)
-            # content = await page.content()
             await browser.close()
     
     async def is_net_ok(self) -> bool:
@@ -42,14 +55,23 @@ class Network_maintain:
                 page = await context.new_page()
                 await page.goto("http://baidu.com/")
                 if self.url == page.url:
+                    print("检测功能发力中...")
                     await page.fill('#username', self.username)
                     await page.fill('#password', self.password)
                     await page.click('#login-account')
                     await page.wait_for_timeout(5000)
-                    # content = await page.content()
                     await browser.close()
                     return True
                 elif page.url == "http://baidu.com/":
+                    return True
+                elif page.url.startswith("http://172.30.21.100/"):
+                    self.url = page.url
+                    print(f"检测功能发力中...更新登录地址为{self.url}")
+                    await page.fill('#username', self.username)
+                    await page.fill('#password', self.password)
+                    await page.click('#login-account')
+                    await page.wait_for_timeout(5000)
+                    await browser.close()
                     return True
         except:
             return False
---
+++ start.bat
@@ -0,0 +1,3 @@
+netsh wlan connect name=WHUT-DORM
+echo "自动连接网络成功,现在开始执行维护逻辑"
+"D:/Program Files/anaconda/envs/python/python.exe" d:/Git/WHUT-WLAN-OPT/internet_maintain.py
